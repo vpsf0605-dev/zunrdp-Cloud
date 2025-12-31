9be7c071d65b0d002bd672d5f80dcf834511a7d5
vpsf0605-dev <vpsf0605@gmail.com>
2025-12-31 22:59:07 +0700

    Update run_vm.ps1

--- run_vm.ps1
+++ run_vm.ps1
@@ -1,32 +1,21 @@
-param (
-    [string]$Owner = "Admin",
-    [string]$MachineID = "Zun-VM"
-)
-
+param ([string]$Owner, [string]$MachineID)
 $url = "https://zunrdp-default-rtdb.asia-southeast1.firebasedatabase.app/vms/$MachineID.json"
-$startTime = [DateTimeOffset]::Now.ToUnixTimeMilliseconds()
 
 while($true) {
     try {
-        $current = Invoke-RestMethod -Uri $url -Method Get
-        if ($current.command -eq "kill") {
-            shutdown /s /f /t 0
-            break
-        }
-        
         $cpu = (Get-WmiObject Win32_Processor | Measure-Object -Property LoadPercentage -Average).Average
         $os = Get-WmiObject Win32_OperatingSystem
         $ram = [Math]::Round(((($os.TotalVisibleMemorySize - $os.FreePhysicalMemory) / $os.TotalVisibleMemorySize) * 100), 1)
+        $ip = (& "C:\Program Files\Tailscale\tailscale.exe" ip -4)
 
         $data = @{
-            id = $MachineID; ip = (tailscale ip -4); owner = $Owner;
+            id = $MachineID; ip = "$ip"; owner = "$Owner";
             cpu = [Math]::Round($cpu, 1); ram = $ram;
-            lastSeen = [DateTimeOffset]::Now.ToUnixTimeMilliseconds();
-            startTime = $startTime
+            lastSeen = [DateTimeOffset]::Now.ToUnixTimeMilliseconds()
         } | ConvertTo-Json
         
-        Invoke-RestMethod -Uri $url -Method Patch -Body $data
-    } catch {}
+        Invoke-RestMethod -Uri $url -Method Put -Body $data
+    } catch { Write-Host "Loi: $_" }
     Start-Sleep -Seconds 5
 }
 
