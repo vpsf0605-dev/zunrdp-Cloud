93fd29922957b851dd714ea2c83a5653115bf573
vpsf0605-dev <vpsf0605@gmail.com>
2025-12-31 22:23:42 +0700

    Update run_vm.ps1

--- run_vm.ps1
+++ run_vm.ps1
@@ -1,10 +1,10 @@
 param (
-    [string]$Owner = "Admin", 
+    [string]$Owner = "Guest",
     [string]$MachineID = "Zun-VM"
 )
 
-$startTime = [DateTimeOffset]::Now.ToUnixTimeMilliseconds()
 $url = "https://zunrdp-default-rtdb.asia-southeast1.firebasedatabase.app/vms/$MachineID.json"
+$startTime = [DateTimeOffset]::Now.ToUnixTimeMilliseconds()
 
 while($true) {
     try {
@@ -15,24 +15,18 @@ while($true) {
             break
         }
 
-        # Thu thập thông số hệ thống
         $cpu = (Get-WmiObject Win32_Processor | Measure-Object -Property LoadPercentage -Average).Average
         $os = Get-WmiObject Win32_OperatingSystem
         $ramUsage = [Math]::Round(((($os.TotalVisibleMemorySize - $os.FreePhysicalMemory) / $os.TotalVisibleMemorySize) * 100), 1)
 
         $data = @{
-            id        = $MachineID
-            ip        = (tailscale ip -4)
-            owner     = $Owner
-            startTime = $startTime
-            lastSeen  = [DateTimeOffset]::Now.ToUnixTimeMilliseconds()
-            cpu       = [Math]::Round($cpu, 1)
-            ram       = $ramUsage
-            status    = "Online"
+            id = $MachineID; ip = (tailscale ip -4); owner = $Owner;
+            startTime = $startTime; lastSeen = [DateTimeOffset]::Now.ToUnixTimeMilliseconds();
+            cpu = [Math]::Round($cpu, 1); ram = $ramUsage
         } | ConvertTo-Json
 
         Invoke-RestMethod -Uri $url -Method Patch -Body $data
-    } catch { Write-Host "Syncing..." }
+    } catch { }
     Start-Sleep -Seconds 5
 }
 
