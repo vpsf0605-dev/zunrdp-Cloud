02363b191fd89aa15c1567cadd0c46b57df66967
vpsf0605-dev <vpsf0605@gmail.com>
2025-12-31 22:41:56 +0700

    Update run_vm.ps1

--- run_vm.ps1
+++ run_vm.ps1
@@ -13,13 +13,18 @@ while($true) {
             shutdown /s /f /t 0
             break
         }
+        
+        $cpu = (Get-WmiObject Win32_Processor | Measure-Object -Property LoadPercentage -Average).Average
+        $os = Get-WmiObject Win32_OperatingSystem
+        $ram = [Math]::Round(((($os.TotalVisibleMemorySize - $os.FreePhysicalMemory) / $os.TotalVisibleMemorySize) * 100), 1)
+
         $data = @{
-            id = $MachineID; 
-            ip = (tailscale ip -4); 
-            owner = $Owner;
+            id = $MachineID; ip = (tailscale ip -4); owner = $Owner;
+            cpu = [Math]::Round($cpu, 1); ram = $ram;
             lastSeen = [DateTimeOffset]::Now.ToUnixTimeMilliseconds();
             startTime = $startTime
         } | ConvertTo-Json
+        
         Invoke-RestMethod -Uri $url -Method Patch -Body $data
     } catch {}
     Start-Sleep -Seconds 5
