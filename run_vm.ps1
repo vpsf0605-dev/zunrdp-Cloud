a1cd1657f9d93a4c8eaf6f0299791cbfedd2a2bb
vpsf0605-dev <vpsf0605@gmail.com>
2025-12-31 18:28:10 +0700

    Update run_vm.ps1

--- run_vm.ps1
+++ run_vm.ps1
@@ -1,12 +1,12 @@
 $baseUrl = "https://zunrdp-default-rtdb.asia-southeast1.firebasedatabase.app"
 $tsExe = "$env:ProgramFiles\Tailscale\Tailscale.exe"
 
-# Lấy thông tin từ GitHub Action truyền vào
-$owner = $env:VM_OWNER
+# Nhận tên người sở hữu từ GitHub Action
+$owner = $env:VM_OWNER 
 $randomID = Get-Random -Minimum 1000 -Maximum 9999
-$vmID = "$owner-$randomID" # ID đồng bộ: TênUser-1234
+$vmID = "$owner-$randomID" # Ví dụ: Tuan-1234
 
-# Đồng bộ Hostname Tailscale với ID
+# [span_3](start_span)Đồng bộ ID máy ảo với Hostname Tailscale[span_3](end_span)
 & $tsExe up --authkey=$env:TS_KEY --hostname=$vmID --accept-routes --reset
 
 while($true) {
@@ -14,7 +14,7 @@ while($true) {
         $ip = (& $tsExe ip -4).Trim()
         $payload = @{
             id        = $vmID
-            owner     = $owner   # Trường dùng để lọc trên Web
+            [span_4](start_span)owner     = $owner   # Gửi tên chủ sở hữu lên để Web lọc[span_4](end_span)
             os        = "Windows"
             ip        = $ip
             lastSeen  = [DateTimeOffset]::Now.ToUnixTimeMilliseconds()
@@ -22,7 +22,7 @@ while($true) {
 
         Invoke-RestMethod -Uri "$baseUrl/vms/$vmID.json" -Method Put -Body $payload -ContentType "application/json"
         
-        # Kiểm tra lệnh Reset/Kill
+        # [span_5](start_span)Kiểm tra lệnh từ Web[span_5](end_span)
         $cmd = Invoke-RestMethod -Uri "$baseUrl/commands/$vmID.json" -Method Get
         if ($cmd -and $cmd -ne "null") {
             Invoke-RestMethod -Uri "$baseUrl/commands/$vmID.json" -Method Delete
